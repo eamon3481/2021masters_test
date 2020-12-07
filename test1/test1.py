c67145d061b8c1541abc2806ed9c9093a4853145
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2020-12-08 01:11:29 +0900

    [test1]make swich funcution
    word에 저장된 단어를 number의 수만큼 밀어내주는 swich 함수생성

--- test1/test1.py
+++ test1/test1.py
@@ -1,6 +1,24 @@
 import sys 
 sys.stdin=open("test1\input.txt","rt")
 
-a, b, c = input().split()
+word, b, direction = input().split()
 
-print(a, b, c)
+num = int(b)
+
+print(word[0])
+
+def swich(word,number):
+    tem=[0]*(len(word)) 
+    for i in range(len(word)):
+        if 0 <= i+number< len(word):
+            tem[i+number]=word[i]
+        elif i+number < 0:
+            tem[len(word) + i+number]=word[i]
+        elif i+number >= len(word):
+            tem[i+number - len(word)]=word[i]
+    Newword = ""
+    for t in tem:
+        Newword = Newword + t
+    return Newword
+
+print(swich(word,num))
