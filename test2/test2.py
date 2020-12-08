cbea02b51747b41662a9ed823e17ea7148b2837e
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2020-12-08 11:27:46 +0900

    [test2]printCube
    입력된 cube를 출력하는 함수 생성

--- test2/test2.py
+++ test2/test2.py
@@ -2,4 +2,11 @@ import sys
 sys.stdin=open("test2\input.txt","rt")
 
 cube = [list(map(str,input().split())) for x in range(3)]
-print(cube)
+
+def printCube(a):
+    for x in a:
+        for b in x:
+            print(b, end=" ")
+        print("")
+
+printCube(cube)
