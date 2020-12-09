8b3cd7ea8701742cca19143fdee699d102278482
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2020-12-09 11:00:25 +0900

    [test2]make upperL function
    upperL함수만들어서 명령어U 실행하게 만듬

--- test2/test2.py
+++ test2/test2.py
@@ -1,8 +1,9 @@
+from os import close
 import sys 
-sys.stdin=open("test2\input.txt","rt")
+##f=open("test2\input.txt","rt")
 
-cube = [list(map(str,input().split())) for x in range(3)]
-
+##cube = [list(map(str,input().split())) for x in range(3)]
+cube = [["R","R","W"],["G","C","W"],["G","B","B"]]
 def printCube(a):
     for x in a:
         for b in x:
@@ -10,3 +11,17 @@ def printCube(a):
         print("")
 
 printCube(cube)
+
+#order = input().split()
+
+def upperL(c):
+    tem = c[0]
+    for x in range(1,3):
+        print(x)
+        c[0][x-1] = tem[x] 
+    c[0][2] = tem[0] 
+    print(c[0])
+
+
+upperL(cube)
+print(cube)
