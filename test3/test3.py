dc79636f45045003d402153e86f284a4b7447e91
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2020-12-11 02:37:13 +0900

    [test3]cclockwise,clockwise
    면을 시계방향으로 반시계방향으로 돌려주는 함수 작성

--- test3/test3.py
+++ test3/test3.py
@@ -16,6 +16,8 @@ def printcube():
             print(" ", end="")
         print(" ")
     print("\n")
+
+
     for x in cube[5]:
         for b in x:
             print(b, end=" ")
@@ -23,35 +25,56 @@ def printcube():
     print("\n")
 
 
+def ClockWise_surface(c):
+    tem = tuple(c[0])
+    c[0][1] = c[1][2]
+    c[0][2] = c[2][2]
+    c[1][2] = c[2][1]
+    c[2][2] = c[2][0]
+    c[2][1] = c[1][0]
+    c[2][0] = tem[0]
+    c[1][0] = tem[1]
+    c[0][0] = tem[2]
 
+def CClockWise_surface(c):
+    tem = tuple(c[0])
+    c[0][1] = c[1][0]
+    c[0][0] = c[2][0]
+    c[1][0] = c[2][1]
+    c[2][0] = c[2][2]
+    c[2][1] = c[1][2]
+    c[2][2] = tem[2]
+    c[1][2] = tem[1]
+    c[0][2] = tem[0]
 
 
 
 printcube()
 
 def ClockWise_U():
+    ClockWise_surface(cube[0])
     tem = cube[1][0]
     for i in range(3):
         cube[1+i][0] = cube[2+i][0]
     cube[4][0]= tem
 
 def ClockWise_D():
+    ClockWise_surface(cube[5])
     tem = cube[1][2]
     for i in range(3):
         cube[1+i][2] = cube[2+i][2]
     cube[4][2]= tem
 
 def counterClockWise_U():
+    CClockWise_surface(cube[0])
     tem = cube[4][0]
     for i in range(3):
         cube[4-i][0] = cube[3-i][0]
     cube[1][0]= tem
 
 def counterClockWise_D():
+    CClockWise_surface(cube[5])
     tem = cube[4][2]
     for i in range(3):
         cube[4-i][2] = cube[3-i][2]
     cube[1][2]= tem
-
-counterClockWise_U()
-printcube()
