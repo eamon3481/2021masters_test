9e7056301dc307df75c946b37d2e72d203577e9d
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2020-12-11 15:00:32 +0900

    [test3]make main function
    if문을 이용해서 명령어 입력

--- test3/test3.py
+++ test3/test3.py
@@ -85,8 +85,8 @@ def CClockWise_L():
     tem = ( cube[2][0][0], cube[2][1][0], cube[2][2][0])
     for x in range(3):
         cube[2][x][0] = cube[5][x][0]
-        cube[5][x][0] = cube[4][x][0]
-        cube[4][x][0] = cube[0][x][0]
+        cube[5][x][0] = cube[4][x][2]
+        cube[4][x][2] = cube[0][x][0]
         cube[0][x][0] = tem[x]
     CClockWise_surface(cube[1])
 
@@ -94,8 +94,8 @@ def ClockWise_R():
     tem = ( cube[2][0][2], cube[2][1][2], cube[2][2][2])
     for x in range(3):
         cube[2][x][2] = cube[5][x][2]
-        cube[5][x][2] = cube[4][x][2]
-        cube[4][x][2] = cube[0][x][2]
+        cube[5][x][2] = cube[4][x][0]
+        cube[4][x][0] = cube[0][x][2]
         cube[0][x][2] = tem[x]
     ClockWise_surface(cube[3])
 
@@ -103,8 +103,8 @@ def ClockWise_L():
     tem = ( cube[2][0][0], cube[2][1][0], cube[2][2][0])
     for x in range(3):
         cube[2][x][0] = cube[0][x][0]
-        cube[0][x][0] = cube[4][x][0]
-        cube[4][x][0] = cube[5][x][0]
+        cube[0][x][0] = cube[4][x][2]
+        cube[4][x][2] = cube[5][x][0]
         cube[5][x][0] = tem[x]
     ClockWise_surface(cube[1])
 
@@ -112,9 +112,98 @@ def CClockWise_R():
     tem = ( cube[2][0][2], cube[2][1][2], cube[2][2][2])
     for x in range(3):
         cube[2][x][2] = cube[0][x][2]
-        cube[0][x][2] = cube[4][x][2]
-        cube[4][x][2] = cube[5][x][2]
+        cube[0][x][2] = cube[4][x][0]
+        cube[4][x][0] = cube[5][x][2]
         cube[5][x][2] = tem[x]
     CClockWise_surface(cube[3])
 
-printcube()
+def Clockwise_F():
+    tem = tuple(cube[0][2])
+    for x in range(3):
+        cube[0][2][x] = cube[1][x][2]
+        cube[1][x][2] = cube[5][0][x]
+        cube[5][0][x] = cube[3][x][0]
+        cube[3][x][0] = tem[x]
+    ClockWise_surface(cube[2])
+
+def CClockwise_F():
+    tem = tuple(cube[0][2])
+    for x in range(3):
+        cube[0][2][x] = cube[3][x][0]
+        cube[3][x][0] = cube[5][0][x]
+        cube[5][0][x] = cube[1][x][2]
+        cube[1][x][2] = tem[x]
+    CClockWise_surface(cube[2])
+
+def Clockwise_B():
+    tem = tuple(cube[0][0])
+    for x in range(3):
+        cube[0][2][x] = cube[3][x][2]
+        cube[3][x][2] = cube[5][2][x]
+        cube[5][2][x] = cube[1][x][0]
+        cube[1][x][0] = tem[x]
+    ClockWise_surface(cube[4])
+
+def CClockwise_B():
+    tem = tuple(cube[0][0])
+    for x in range(3):
+        cube[0][2][x] = cube[1][x][0]
+        cube[1][x][0] = cube[5][2][x]
+        cube[5][2][x] = cube[3][x][2]
+        cube[3][x][2] = tem[x]
+    CClockWise_surface(cube[4])
+
+def main(orders):
+    for order in orders:
+        if order == "U":
+            print("\n", order)
+            ClockWise_U()
+            printcube()
+        elif order == "U'":
+            print("\n", order)
+            counterClockWise_U()
+            printcube()  
+        elif order == "D":
+            print("\n", order)
+            ClockWise_D()
+            printcube()
+        elif order == "D'":
+            print("\n", order)
+            counterClockWise_D()
+            printcube()
+        elif order == "L":
+            print("\n", order)
+            ClockWise_L()
+            printcube()
+        elif order == "L'":
+            print("\n", order)
+            CClockWise_L()
+            printcube()
+        elif order == "R":
+            print("\n", order)
+            ClockWise_R()
+            printcube()
+        elif order == "R'":
+            print("\n", order)
+            CClockWise_R()
+            printcube()
+        elif order == "F":
+            print("\n", order)
+            Clockwise_F()
+            printcube()
+        elif order == "F'":
+            print("\n", order)
+            CClockwise_F()
+            printcube()
+        elif order == "B":
+            print("\n", order)
+            Clockwise_B()
+            printcube()
+        elif order == "B'":
+            print("\n", order)
+            CClockwise_B()
+            printcube()
+
+
+        
+
